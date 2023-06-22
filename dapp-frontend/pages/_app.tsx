eba94dbee8e61f860c6d5f26ad011cf9284f0864
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-06-22 21:47:51 +0100

    update: removed nav and footer

--- dapp-frontend/pages/_app.tsx
+++ dapp-frontend/pages/_app.tsx
@@ -21,9 +21,7 @@ function getLibrary(provider: any) {
 const AppContent = ({ children }: any) => {
   return (
     <div className="bg-[#0c0c0c] min-h-screen scroll-smooth flex flex-col w-screen overflow-hidden relative">
-      <Header />
       <div className="overflow-auto flex-1">{children}</div>
-      <Footer />
     </div>
   );
 };
